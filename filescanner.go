bfa1578f5f68060a09dddcad58a23640116c784a
Christian van der Leeden <christian@vanderleeden.de>
2019-12-24 15:39:27 +0100

    enabled go modules and added example postgres connection support

---
+++ filescanner.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jackc/pgx/v4"
+	"os"
+	"context"
+)
+
+func main() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connection to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var name string
+	var weight int64
+	err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(name, weight)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module vanderleeden.de/filescanner
+
+go 1.13
+
+require github.com/jackc/pgx/v4 v4.1.2
